a3f868be15cdd67199474c4d19cc2225014eee10
Ralph Meijer <ralphm@ik.nu>
2006-07-27 19:39:01 +0000

    Use Twisted's new plugins mechanism.
    --HG--
    extra : convert_revision : svn%3A90f377ac-1a04-0410-ba68-e473ea136fe9/trunk%4025

---
+++ twisted/plugins/mimir_aggregator.py
@@ -0,0 +1,9 @@
+from twisted.scripts.mktap import _tapHelper
+
+MimirAggregator = _tapHelper(
+        "Mimir Aggregator",
+        "aggregator.tap",
+        "Mimir Feed Aggregator and Feeder",
+        "aggregator")
+
+
