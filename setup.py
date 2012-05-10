cca6cbd1de7383d7fdb1e57fdb7f08378a5cab9c
Ralph Meijer <ralphm@ik.nu>
2012-05-10 16:04:58 +0200

    Make sure 'twisted' doesn't appear in toplevel.txt. Reload twisted plugins.

--- setup.py
+++ setup.py
@@ -6,6 +6,28 @@
 from setuptools import setup
 from mimir import __version__
 
+# Make sure 'twisted' doesn't appear in top_level.txt
+
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
+
 setup(name='mimir',
       version=__version__,
       description=u'Mimir daemons',
@@ -28,3 +50,13 @@ setup(name='mimir',
                           'simplejson',
                           'FeedParser'],
 )
+
+# Make Twisted regenerate the dropin.cache, if possible.  This is necessary
+# because in a site-wide install, dropin.cache cannot be rewritten by
+# normal users.
+try:
+    from twisted.plugin import IPlugin, getPlugins
+except ImportError:
+    pass
+else:
+    list(getPlugins(IPlugin))
